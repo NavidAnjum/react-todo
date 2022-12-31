f4fe175647ceedc86fe505c44c02e51a0c412880
Navid Anjum <navids92@gmail.com>
2022-12-31 16:11:20 +0600

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Another from './Another'
 
 function App() {
+  const [count,setCount]=useState(0)
+  const someStyle={
+    background:'green',
+    color:'white',
+    fontSize:'28px',
+    fontWeight:'blod'
+
+  }
+function increment(){
+  setCount(prevCount=>prevCount+1);
+}
+function decrement(){
+  setCount(prevCount=>prevCount-1);
+}
+
   return (
     <div className="App">
+     
       <header className="App-header">
+      <Another name="Navid"></Another>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <span>{count}</span>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
